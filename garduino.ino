fb68285b37f3cf619fa2400cea352b52fb61903d
Stephen Laide <stephenlaide@gmail.com>
2020-03-16 19:46:17 +0000

    Implement humidity and temperature reading and compute heat index

--- garduino.ino
+++ garduino.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DHT.h"
 
+#define DHT_PIN 4
+
+DHT dht(DHT_PIN, DHT22);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor"));
+    return;
+  }
+  
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C  Heat index: "));
+  Serial.print(hic);
+  Serial.println(F("°C "));
 }
