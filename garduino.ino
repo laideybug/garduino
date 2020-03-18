6fe6df0c9db3a5f16ea3427e1492bbe30d01f896
Stephen Laide <stephenlaide@gmail.com>
2020-03-18 11:08:13 +0000

    Implement light level reading from BH1750 sensor module

--- garduino.ino
+++ garduino.ino
@@ -1,12 +1,20 @@
+#include <BH1750.h>
 #include "DHT.h"
+#include <Wire.h>
 
+// DHT
 #define DHT_PIN 4
 
 DHT dht(DHT_PIN, DHT22);
 
+// BH1750
+BH1750 lightMeter;
+
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
   dht.begin();
+  lightMeter.begin();
 }
 
 void loop() {
@@ -14,13 +22,8 @@ void loop() {
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor"));
-    return;
-  }
-  
   float hic = dht.computeHeatIndex(t, h, false);
+  float lux = lightMeter.readLightLevel(true);
 
   Serial.print(F("Humidity: "));
   Serial.print(h);
@@ -28,5 +31,8 @@ void loop() {
   Serial.print(t);
   Serial.print(F("°C  Heat index: "));
   Serial.print(hic);
-  Serial.println(F("°C "));
+  Serial.print(F("°C"));
+  Serial.print("  Light: ");
+  Serial.print(lux);
+  Serial.println(" lx ");
 }
